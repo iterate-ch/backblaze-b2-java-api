86e846a2af2c84bfd2222b8fb20516d1dc070723
David Kocher <dkocher@iterate.ch>
2016-05-24 17:55:04 +0200

    Add test with X-Bz-Test-Mode

--- src/test/java/synapticloop/b2/request/B2UploadAndDeleteFileRequestTest.java
+++ src/test/java/synapticloop/b2/request/B2UploadAndDeleteFileRequestTest.java
@@ -2,6 +2,7 @@ package synapticloop.b2.request;
 
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
+import synapticloop.b2.exception.B2ApiException;
 import synapticloop.b2.helper.B2TestHelper;
 import synapticloop.b2.response.B2BucketResponse;
 import synapticloop.b2.response.B2DeleteFileVersionResponse;
@@ -40,6 +41,40 @@ public class B2UploadAndDeleteFileRequestTest {
 		B2DeleteFileVersionResponse b2DeleteFileVersionResponse = new B2DeleteFileVersionRequest(HttpClients.createDefault(), B2TestHelper.getB2AuthorizeAccountResponse(), fileName, fileId).getResponse();
 		assertEquals(fileName, b2DeleteFileVersionResponse.getFileName());
 		assertEquals(fileId, b2DeleteFileVersionResponse.getFileId());
+
+		B2TestHelper.deleteBucket(privateBucket.getBucketId());
+	}
+
+	@Test
+	public void testFileUploadFailureTestMode() throws Exception {
+		B2BucketResponse privateBucket = B2TestHelper.createRandomPrivateBucket();
+		B2GetUploadUrlResponse b2GetUploadUrlResponse = B2TestHelper.getUploadUrl(privateBucket.getBucketId());
+		File file = File.createTempFile("backblaze-api-test", ".txt");
+		FileWriter fileWriter = new FileWriter(file);
+		fileWriter.write("hello world!");
+		fileWriter.flush();
+		fileWriter.close();
+		while (true) {
+			try {
+				final B2UploadFileRequest request = new B2UploadFileRequest(HttpClients.createDefault(),
+						B2TestHelper.getB2AuthorizeAccountResponse(), b2GetUploadUrlResponse, file.getName(), file,
+						ChecksumHelper.calculateSha1(file));
+				request.addHeader("X-Bz-Test-Mode", "fail_some_uploads");
+				B2FileResponse b2UploadFileResponse = request.getResponse();
+
+				String fileName = b2UploadFileResponse.getFileName();
+				String fileId = b2UploadFileResponse.getFileId();
+
+				// now we need to delete the file as well to clean up after ourselves
+
+				B2DeleteFileVersionResponse b2DeleteFileVersionResponse = new B2DeleteFileVersionRequest(HttpClients.createDefault(), B2TestHelper.getB2AuthorizeAccountResponse(), fileName, fileId).getResponse();
+				assertEquals(fileName, b2DeleteFileVersionResponse.getFileName());
+				assertEquals(fileId, b2DeleteFileVersionResponse.getFileId());
+			} catch (B2ApiException e) {
+				assertEquals(503, e.getStatus());
+				break;
+			}
+		}
 
 		B2TestHelper.deleteBucket(privateBucket.getBucketId());
 	}
